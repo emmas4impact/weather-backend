4ad1e632a88140bf5ddcad3d0bf6e2b2ad4231ad
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-04 14:47:25 +0300

    Update Schema.js

--- weather/Schema.js
+++ weather/Schema.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 const { Schema } = require("mongoose")
 const v = require("validator")
+const bcrypt = require("bcrypt");
+
 const loginSchema = new Schema({
     name: {
         type: String,
@@ -28,9 +30,27 @@ const loginSchema = new Schema({
         minlength: 7,
       },
 })
+loginSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+  
+    delete userObject.password
+    delete userObject.__v
+  
+    return userObject
+  }
+loginSchema.statics.findByCredentials = async (email, password)=>{
+    const user = await LoginModel.findOne({email})
+    console.log(user)
+    const isMatch = await bcrypt.compare(password, user.password)
+    
+    if (!isMatch) {
+        const err = new Error("Unable to login")
+        err.httpStatusCode = 401
+        throw err
+    }
 
-loginSchema.statics.findByCreadentials = async (username, password)=>{
-    const user = await loginSchema.f
+  return user
 }
 
 const LoginModel = mongoose.model("login", loginSchema);
