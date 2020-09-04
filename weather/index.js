8240b84e791da382fd6af93510d7849be99e338d
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-04 14:47:21 +0300

    Update index.js

--- weather/index.js
+++ weather/index.js
@@ -1,6 +1,7 @@
 const express =require("express")
 const LoginModel = require("./Schema")
-const authenticate = require("../auth/authTools")
+const {authenticate }= require("../auth/authTools")
+const {authorize} = require("../middlewares/authorize")
 const router = express.Router()
 const bcrypt = require("bcrypt");
 const passport = require("passport");
@@ -24,19 +25,45 @@ router.post("/register", async (req, res) => {
       res.send(error.errors);
     }
   });
-
+router.get("/", authorize, async(req, res, next)=>{
+    try {
+        const users = await LoginModel.find(req.query)
+        res.send({
+            data: users,
+            total: users.length
+        })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
 router.post("/login", async(req, res, next)=>{
     try {
         const {email, password}= req.body
         const user = await LoginModel.findByCredentials(email, password)
-        
-        const token  =await authenticate(user)
+        console.log(user)
+        const token  = await authenticate(user)
+        res.cookie("accessToken", token, {
+            path: "/",
+            httpOnly: true,
+            sameSite: true,
+          })
         res.send(token)
     } catch (error) {
         
     }
     
 })
-
+// router.post("/logout", authorize, async (req, res, next) => {
+//     try {
+//       req.user.refreshTokens = req.user.refreshTokens.filter(
+//         (t) => t.token !== req.body.refreshToken
+//       )
+//       await req.user.save()
+//       res.send()
+//     } catch (err) {
+//       next(err)
+//     }
+//   })
 
 module.exports = router;
